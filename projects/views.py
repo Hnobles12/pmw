f82ccf9d54e50f2ae0ddb4e8fcccb65235e0069a
HN <hnobles12@gmail.com>
2022-11-04 12:44:50 -0500

    Added working integration of VueJS to project index

--- projects/views.py
+++ projects/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    ctx = {}
+    ctx = {
+        "message":"Howdy Vue!"
+    }
     return render(request, 'index.html', ctx)
