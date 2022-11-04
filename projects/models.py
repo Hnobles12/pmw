449b763a37dbeccead9ead03e6d794b6a06c8731
HN <hnobles12@gmail.com>
2022-11-04 13:07:24 -0500

    Created Project model and registered with admin

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    created = models.DateTimeField()
+    modified = models.DateTimeField()
+    todos = models.JSONField()
+    
--- projects/views.py
+++ projects/views.py
@@ -6,8 +6,18 @@ def main(request):
     }
     return render(request, 'main.html', ctx)
 
+
+import json
+class Test:
+    def __init__(self):
+        self.number = 100
+        self.text = "Test Text"
+    def as_json(self):
+        return json.dumps(self.__dict__)
+
 def vue_test(request):
     ctx = {
+        "obj": Test().as_json(),
         "message":"Howdy Vue!"
     }
     return render(request, 'vue_test.html', ctx)
